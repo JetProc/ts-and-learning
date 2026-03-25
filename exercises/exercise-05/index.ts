94cdeea96f4a4c36c375ef282118fd67548e316f
JetProc <soonjae8297@gmail.com>
2026-03-25 13:28:34 +0900

    refactor: exercise-05 보너스 문제 풀이 완료

--- exercises/exercise-05/index.ts
+++ exercises/exercise-05/index.ts
@@ -94,10 +94,10 @@ export function logPerson(person: Person) {
 
 export function filterUsers(
   persons: Person[],
-  criteria: Partial<User>
+  criteria: Partial<Omit<User, 'type'>
 ): User[] {
   return persons.filter(isUser).filter((user) => {
-    const criteriaKeys = Object.keys(criteria) as (keyof User)[];
+    const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, 'type'>)[];
     return criteriaKeys.every((fieldName) => {
       return user[fieldName] === criteria[fieldName];
     });
